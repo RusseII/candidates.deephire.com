a6578e734f0a94bf04ff93815ba5f10de9ac041b
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-02-05 22:43:30 -0500

    redirect for rupert

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import styles from './index.css';
+import qs from 'qs';
+import { router } from 'umi';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>Hmm... Howed you get here??</li>
-        <li>Email me at Russell@deephire.com if you are lost!</li>
-      </ul>
-    </div>
-  );
+import React, { Component } from 'react';
+
+export default class Shortlist extends Component {
+  componentDidMount() {
+    const { location } = this.props;
+
+    const id = qs.parse(location.search)['?id'];
+    if (id == '5bc4b7dc74468900083644fa')
+      router.push(`/candidate?shortlist=5c5a518ff2ee174b76f2dc58&num=0`);
+  }
+
+  render() {
+    return (
+      <div className={styles.normal}>
+        <div className={styles.welcome} />
+        <ul className={styles.list}>
+          <li>Hmm... Howed you get here??</li>
+          <li>Email me at Russell@deephire.com if you are lost!</li>
+        </ul>
+      </div>
+    );
+  }
 }
