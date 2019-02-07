d171e31f59245d9f9510c26a30eadcc6ad045c4b
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-02-07 08:51:39 -0500

    hardcode allectio

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,49 @@
 import { Col, Row, Layout } from 'antd';
+import React, { Component } from 'react';
+import qs from 'qs';
+import { fetchShortlist } from '@/services/api';
 const Header = Layout.Header;
 
-function BasicLayout(props) {
-  return (
-    <div style={{ backgroundColor: '#F0F2F5', padding: '0px' }}>
-      <Header style={{ backgroundColor: 'white' }}>
-        {' '}
-        <Row type="flex" style={{ height: '60%' }} justify="space-between">
-          <Col>Shared by: Tempo</Col>
-          <Col>
-            <img
-              src="https://s3.amazonaws.com/deephire/importantImages/suzanneTempoLogo.png"
-              alt="Forge"
-              height="100%"
-            />
-          </Col>
-        </Row>
-      </Header>
-      <div>
+export default class BasicLayout extends Component {
+  componentDidMount() {
+    const { location } = this.props;
+    const id = qs.parse(location.search)['?shortlist'];
+    console.log(id);
+    fetchShortlist(id).then(r =>
+      this.setState({
+        shortListData: r[0],
+        id,
+      })
+    );
+  }
 
+  render() {
+    return (
+      <div style={{ backgroundColor: '#F0F2F5', padding: '0px' }}>
+        <Header style={{ backgroundColor: 'white' }}>
+          {' '}
+          <Row type="flex" style={{ height: '60%' }} justify="space-between">
+            <Col>Shared by: Tempo</Col>
+            <Col>
+              {this.state.shortListData.sharedBy === 'mskalak@allectiomedical.com' ? (
+                <img
+                  src="https://s3.amazonaws.com/deephire/allectio.png"
+                  alt="Allectio Medical"
+                  height="100%"
+                />
+              ) : (
+                <img
+                  src="https://s3.amazonaws.com/deephire/importantImages/suzanneTempoLogo.png"
+                  alt="Forge"
+                  height="100%"
+                />
+              )}
+            </Col>
+          </Row>
+        </Header>
+        <div />
+        <div style={{ height: '100vh', padding: '20px' }}>{this.props.children}</div>
       </div>
-      <div style={{ height: "100vh", padding: "20px" }}>
-        {props.children}
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default BasicLayout;
