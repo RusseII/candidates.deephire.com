05e2312429cec21ce212e1d0927726c19880b0e8
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-02-14 08:59:56 -0500

    support ie11

--- .umirc.js
+++ .umirc.js
@@ -2,6 +2,9 @@
 // ref: https://umijs.org/config/
 export default {
   treeShaking: true,
+   targets: {
+    ie: 11,
+  },
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
