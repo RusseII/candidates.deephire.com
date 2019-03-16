39b9cf39d5ea912d54e0b32065e188e23c1ab7db
Russell Ratcliffe <rratcliffe57@gmail.com>
2019-03-16 08:19:54 -0400

    Add dynamic branding based on mongodb

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,61 +1,41 @@
 import { Col, Row, Layout } from 'antd';
-import React, {  useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import qs from 'qs';
-import { fetchShortlist } from '@/services/api';
+import { fetchShortlist, fetchCompanyInfo } from '@/services/api';
 const Header = Layout.Header;
 
 const BasicLayout = ({ location, children }) => {
-  const [shortListData, setShortListData] = useState({})
+  const [shortListData, setShortListData] = useState({});
+  const [companyInfo, setCompanyInfo] = useState({ companyName: 'Loading...', logo: '' });
   const id = qs.parse(location.search)['?shortlist'];
 
-
   useEffect(() => {
-    fetchShortlist(id).then(r =>
-        setShortListData(r[0])
-        
-      )
-  }, [])
+    fetchShortlist(id).then(r => {
+      setShortListData(r[0]);
+      fetchCompanyInfo(r[0].createdBy).then(r => setCompanyInfo(r || {}));
+    });
+  }, []);
 
-    return (
-      <div style={{ backgroundColor: '#F0F2F5', padding: '0px' }}>
-        <Header style={{ backgroundColor: 'white' }}>
-          {' '}
-          <Row type="flex" style={{ height: '60px' }} justify="space-between">
-            <Col>
-              Shared by:{' '}
-              {shortListData.createdBy === 'mskalak@allectiomedical.com'
-                ? 'Allectio Medical'
-                : shortListData.createdBy === 'dbrennan@assistinghands.com' ? "Assisting Hands":'Tempo'}
-            </Col>
-            <Col>
-              {shortListData.createdBy === 'mskalak@allectiomedical.com' ? (
-                <img
-                  src="https://s3.amazonaws.com/deephire/allectio.png"
-                  alt="Allectio Medical"
-                  height="50px"
-                />
-              ) : 
-                
-                shortListData.createdBy === 'dbrennan@assistinghands.com' ? 
+  console.log(companyInfo);
+  return (
+    <div style={{ backgroundColor: '#F0F2F5', padding: '0px' }}>
+      <Header style={{ backgroundColor: 'white' }}>
+        {' '}
+        <Row type="flex" style={{ height: '60px' }} justify="space-between">
+          <Col>Shared by: {companyInfo.companyName || 'DeepHire'}</Col>
+          <Col>
+            <img
+              src={companyInfo.logo || 'https://s3.amazonaws.com/deephire/dh_vertical.png'}
+              alt={companyInfo.companyName}
+              height="50px"
+            />
+          </Col>
+        </Row>
+      </Header>
+      <div />
+      <div style={{ height: '100vh', padding: '20px' }}>{children}</div>
+    </div>
+  );
+};
 
-              (
-                <img
-                  src="https://s3.amazonaws.com/deephire/logos/AssistingHandsLogo.jpg"
-                  alt="assistingHands"
-                  height="50px"
-                />
-                  ) : <img
-                    src="https://s3.amazonaws.com/deephire/importantImages/suzanneTempoLogo.png"
-                    alt="Forge"
-                    height="50px"
-                  />}
-            </Col>
-          </Row>
-        </Header>
-        <div />
-        <div style={{ height: '100vh', padding: '20px' }}>{children}</div>
-      </div>
-    );
-  }
-
-  export default BasicLayout
+export default BasicLayout;
